37e2b2fe5690201d95d5a1e52577f689673f9b76
zhangxuelin <zhangxuelin@tcminplay.com>
2020-05-22 14:08:14 +0800

    add two nums

--- Algorithm/Simple/Program.cs
+++ Algorithm/Simple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Simple.两数之和;
+using Simple.两数相加;
 using System;
 
 namespace Simple
@@ -7,7 +8,17 @@ namespace Simple
     {
         static void Main(string[] args)
         {
-            TwoNumSum.OutPutTowSumArrayIndex_Violence(new int[] { 3, 2, 4 }, 6);
+            //TwoNumSum.OutPutTowSumArrayIndex_Violence(new int[] { 3, 2, 4 }, 6);
+
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(3);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            AddTowNums.AddTwoNumbers(l1,l2);
 
             Console.WriteLine("Hello World!");
         }
---
+++ Algorithm/Simple/两数相加/AddTowNums.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple.两数相加
+{
+    public class ListNode
+    {
+        public int val { get; set; }
+
+        public ListNode next { get; set; }
+
+        public ListNode(int x)
+        {
+            this.val = x;
+        }
+    }
+    public class AddTowNums
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode target = new ListNode(0);              //哑节点
+            ListNode p = l1, q = l2, current = target;
+            int carry = 0;                                  //进位
+            while (p != null || q != null)
+            {
+                int x = p != null ? p.val : 0;
+                int y = q != null ? q.val : 0;
+                int sum = x + y + carry;
+                carry = sum / 10;
+
+                current.next = new ListNode(sum % 10);
+                current = current.next;                    //将当前节点移到下一个节点
+                if (p != null) p = p.next;                                //将p移到下一个几点
+                if (q != null) q = q.next;                                //将q移到下一个节点
+            }
+
+            if (carry > 0)
+            {
+                current.next = new ListNode(carry);
+            }
+            return target.next;
+        }
+    }
+}
