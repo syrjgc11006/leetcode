6f3419338083a2d83b447c9799c988c4085f0121
zhangxuelin <zhangxuelin@tcminplay.com>
2020-05-22 14:40:45 +0800

    double listnode

--- Algorithm/Simple/Program.cs
+++ Algorithm/Simple/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Simple.两数之和;
 using Simple.两数相加;
 using System;
+using System.Collections.Generic;
 
 namespace Simple
 {
@@ -10,15 +11,52 @@ namespace Simple
         {
             //TwoNumSum.OutPutTowSumArrayIndex_Violence(new int[] { 3, 2, 4 }, 6);
 
-            ListNode l1 = new ListNode(2);
-            l1.next = new ListNode(4);
-            l1.next.next = new ListNode(3);
+            //ListNode l1 = new ListNode(2);
+            //l1.next = new ListNode(4);
+            //l1.next.next = new ListNode(3);
 
-            ListNode l2 = new ListNode(5);
-            l2.next = new ListNode(6);
-            l2.next.next = new ListNode(4);
+            //ListNode l2 = new ListNode(5);
+            //l2.next = new ListNode(6);
+            //l2.next.next = new ListNode(4);
 
-            AddTowNums.AddTwoNumbers(l1,l2);
+            //AddTowNums.AddTwoNumbers(l1,l2);
+
+            //LinkedList<int> a = new LinkedList<int>(); LinkedList<int> b = new LinkedList<int>();
+            //LinkedListNode<int> l1 = new LinkedListNode<int>(2);
+            //LinkedListNode<int> l1_1 = new LinkedListNode<int>(4);
+            //LinkedListNode<int> l1_2 = new LinkedListNode<int>(3);
+            //a.AddFirst(l1);
+            //a.AddAfter(l1, l1_1);
+            //a.AddAfter(l1_1, l1_2);
+
+
+            //LinkedListNode<int> l2 = new LinkedListNode<int>(5);
+            //LinkedListNode<int> l2_1 = new LinkedListNode<int>(6);
+            //LinkedListNode<int> l2_2 = new LinkedListNode<int>(4);
+            //b.AddFirst(l2);
+            //b.AddAfter(l2, l2_1);
+            //b.AddAfter(l2_1, l2_2);
+
+            //AddTowNums.AddTwoNumbers(l1, l2);
+
+
+            LinkedList<int> a = new LinkedList<int>(); LinkedList<int> b = new LinkedList<int>();
+            LinkedListNode<int> l1 = new LinkedListNode<int>(2);
+            LinkedListNode<int> l1_1 = new LinkedListNode<int>(4);
+            LinkedListNode<int> l1_2 = new LinkedListNode<int>(3);
+            a.AddFirst(l1);
+            a.AddAfter(l1, l1_1);
+            a.AddAfter(l1_1, l1_2);
+
+
+            LinkedListNode<int> l2 = new LinkedListNode<int>(5);
+            LinkedListNode<int> l2_1 = new LinkedListNode<int>(6);
+            LinkedListNode<int> l2_2 = new LinkedListNode<int>(4);
+            b.AddFirst(l2);
+            b.AddAfter(l2, l2_1);
+            b.AddAfter(l2_1, l2_2);
+
+            AddTowNums.AddTwoNumbers_Pre(l1, l2);
 
             Console.WriteLine("Hello World!");
         }
--- Algorithm/Simple/两数相加/AddTowNums.cs
+++ Algorithm/Simple/两数相加/AddTowNums.cs
@@ -17,6 +17,13 @@ namespace Simple.两数相加
     }
     public class AddTowNums
     {
+        /// <summary>
+        /// 时间复杂度：o(max(x,y)) 计算的时间取决于l1和l2的长度
+        /// 空间复杂度为：o(max(x,y))
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
         public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
             ListNode target = new ListNode(0);              //哑节点
@@ -40,6 +47,70 @@ namespace Simple.两数相加
                 current.next = new ListNode(carry);
             }
             return target.next;
+        }
+
+        public static LinkedListNode<int> AddTwoNumbers(LinkedListNode<int> l1, LinkedListNode<int> l2)
+        {
+            LinkedList<int> target = new LinkedList<int>();              //哑节点
+            var current = new LinkedListNode<int>(0);
+            target.AddFirst(current);
+            LinkedListNode<int> p = l1, q = l2;
+            int carry = 0;                                  //进位
+            while (p != null || q != null)
+            {
+                int x = p != null ? p.Value : 0;
+                int y = q != null ? q.Value : 0;
+                int sum = x + y + carry;
+                carry = sum / 10;
+
+                target.AddAfter(current, sum % 10);
+                //current.Next = new LinkedListNode<int>(sum % 10);
+                current = current.Next;                                   //将当前节点移到下一个节点
+                if (p != null) p = p.Next;                                //将p移到下一个几点
+                if (q != null) q = q.Next;                                //将q移到下一个节点
+            }
+
+            if (carry > 0)
+            {
+                target.AddLast(carry);
+            }
+            return target.First.Next;
+        }
+
+        /// <summary>
+        /// 双向链表存储，可以反序列输出
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public static LinkedListNode<int> AddTwoNumbers_Pre(LinkedListNode<int> l1, LinkedListNode<int> l2)
+        {
+            LinkedList<int> target = new LinkedList<int>();              //哑节点
+            var current = new LinkedListNode<int>(0);
+            target.AddFirst(current);
+            LinkedListNode<int> p = l1, q = l2;
+            int carry = 0;                                  //进位
+            while (p != null || q != null)
+            {
+                int x = p != null ? p.Value : 0;
+                int y = q != null ? q.Value : 0;
+                int sum = x + y + carry;
+                carry = sum / 10;
+
+                target.AddAfter(current, sum % 10);
+                //target.AddBefore(current.Next, sum % 10);
+                //current.Next = new LinkedListNode<int>(sum % 10);
+                current = current.Next;                                   //将当前节点移到下一个节点
+                
+                if (p != null) p = p.Next;                                //将p移到下一个几点
+                if (q != null) q = q.Next;                                //将q移到下一个节点
+            }
+
+            if (carry > 0)
+            {
+                target.AddLast(carry);
+            }
+            return target.Last;
         }
     }
 }
